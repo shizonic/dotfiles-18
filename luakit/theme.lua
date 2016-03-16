f591bdec87bd779556ac88a52bb3336e7cc4794a
mohabaks <mohabaks64@gmail.com>
2016-03-17 00:56:58 +0300

    Update some minor changes to match current theme

--- luakit/theme.lua
+++ luakit/theme.lua
@@ -5,15 +5,15 @@
 local theme = {}
 
 -- Default settings
-theme.font = "Fira Mono 9"
+theme.font = "Droid Sans 9"
 theme.fg   = "#fff"
-theme.bg   = "#101010"
+theme.bg   = "#161212"
 
 -- Genaral colours
 theme.success_fg = "#0f0"
 theme.loaded_fg  = "#b1d631"
 theme.error_fg = "#FFF"
-theme.error_bg = "#101010"
+theme.error_bg = "#161212"
 
 -- Warning colours
 theme.warning_fg = "#F00"
@@ -24,11 +24,11 @@ theme.notif_fg = "#444"
 theme.notif_bg = "#FFF"
 
 -- Menu colours
-theme.menu_fg                   = "#919191"
-theme.menu_bg                   = "#000"
-theme.menu_selected_fg          = "#8181a6"
-theme.menu_selected_bg          = "#151515"
-theme.menu_title_bg             = "#101010"
+theme.menu_fg                   = "#4A3637"
+theme.menu_bg                   = "#151515"
+theme.menu_selected_fg          = "#c0b18b"
+theme.menu_selected_bg          = "#775759"
+theme.menu_title_bg             = "#161212"
 theme.menu_primary_title_fg     = "#f00"
 theme.menu_secondary_title_fg   = "#666"
 
@@ -39,7 +39,7 @@ theme.proxy_inactive_menu_fg    = '#888'
 theme.proxy_inactive_menu_bg    = '#FFF'
 
 -- Statusbar specific
-theme.sbar_fg         = "#b1d631"
+theme.sbar_fg         = "#768748"
 theme.sbar_bg         = "#000"
 
 -- Downloadbar specific
@@ -48,16 +48,16 @@ theme.dbar_bg         = "#000"
 theme.dbar_error_fg   = "#F00"
 
 -- Input bar specific
-theme.ibar_fg           = "#90b0d1"
+theme.ibar_fg           = "#d17b49"
 theme.ibar_bg           = "#000"
 
 -- Tab label
 theme.tab_fg            = "#919191"
 theme.tab_bg            = "#222"
-theme.tab_ntheme        = "#ddd"
-theme.selected_fg       = "#ff9800"
-theme.selected_bg       = "#101010"
-theme.selected_ntheme   = "#ddd"
+theme.tab_ntheme        = "#4A3637"
+theme.selected_fg       = "#c0b18b"
+theme.selected_bg       = "#161212"
+theme.selected_ntheme   = "#4A3637"
 theme.loading_fg        = "#33AADD"
 theme.loading_bg        = "#000"
 
