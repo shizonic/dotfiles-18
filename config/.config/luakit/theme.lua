cb036188ad6aa492641740209aedff3176e33b6c
mohabaks <mohabaks64@gmail.com>
2016-11-15 21:34:31 +0300

    Changed bar colors

--- config/.config/luakit/theme.lua
+++ config/.config/luakit/theme.lua
@@ -6,14 +6,14 @@ local theme = {}
 
 -- Default settings
 theme.font = "tewi 8"
-theme.fg   = "#020202"
-theme.bg   = "#555555"
+theme.fg   = "#020508"
+theme.bg   = "#9e9ecb"
 
 -- Genaral colours
 theme.success_fg = "#d0d0d0"
 theme.loaded_fg  = "#33AADD"
 theme.error_fg = "#FFF"
-theme.error_bg = "#101010"
+theme.error_bg = "#07090d"
 
 -- Warning colours
 theme.warning_fg = "#F00"
@@ -24,11 +24,11 @@ theme.notif_fg = "#444"
 theme.notif_bg = "#FFF"
 
 -- Menu colours
-theme.menu_fg                   = "#555555"
+theme.menu_fg                   = "#9e9ecb"
 theme.menu_bg                   = "#040404"
-theme.menu_selected_fg          = "#020202"
+theme.menu_selected_fg          = "#020508"
 theme.menu_selected_bg          = "#5E468C"
-theme.menu_title_bg             = "#020202"
+theme.menu_title_bg             = "#020508"
 theme.menu_primary_title_fg     = "#477AB3"
 theme.menu_secondary_title_fg   = "#8C4665"
 
@@ -44,22 +44,22 @@ theme.sbar_bg         = "#040404"
 
 -- Downloadbar specific
 theme.dbar_fg         = "#808080"
-theme.dbar_bg         = "#101010"
+theme.dbar_bg         = "#07090d"
 theme.dbar_error_fg   = "#F00"
 
 -- Input bar specific
-theme.ibar_fg           = "#555555"
-theme.ibar_bg           = "#101010"
+theme.ibar_fg           = "#9e9ecb"
+theme.ibar_bg           = "#07090d"
 
 -- Tab label
-theme.tab_fg            = "#555555"
-theme.tab_bg            = "#020202"
-theme.tab_ntheme        = "#555555"
+theme.tab_fg            = "#9e9ecb"
+theme.tab_bg            = "#020508"
+theme.tab_ntheme        = "#9e9ecb"
 theme.selected_fg       = "#5E468C"
-theme.selected_bg       = "#101010"
+theme.selected_bg       = "#07090d"
 theme.selected_ntheme   = "#5E468C"
-theme.loading_fg        = "#555555"
-theme.loading_bg        = "#020202"
+theme.loading_fg        = "#9e9ecb"
+theme.loading_bg        = "#020508"
 
 -- Trusted/untrusted ssl colours
 theme.trust_fg          = "#287373"
