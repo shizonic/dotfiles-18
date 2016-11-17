99d5f61e3732dc19450883847c75fd67cfb6b6a3
mohabaks <mohabaks64@gmail.com>
2016-11-17 09:51:24 +0300

    Cleaned old dots

--- config/.config/luakit/theme.lua
+++ config/.config/luakit/theme.lua
@@ -6,14 +6,14 @@ local theme = {}
 
 -- Default settings
 theme.font = "tewi 8"
-theme.fg   = "#002b36"
-theme.bg   = "#839496"
+theme.fg   = "#020508"
+theme.bg   = "#9e9ecb"
 
 -- Genaral colours
 theme.success_fg = "#d0d0d0"
 theme.loaded_fg  = "#33AADD"
 theme.error_fg = "#FFF"
-theme.error_bg = "#073642"
+theme.error_bg = "#07090d"
 
 -- Warning colours
 theme.warning_fg = "#F00"
@@ -24,13 +24,13 @@ theme.notif_fg = "#444"
 theme.notif_bg = "#FFF"
 
 -- Menu colours
-theme.menu_fg                   = "#839496"
-theme.menu_bg                   = "#073642"
-theme.menu_selected_fg          = "#002b36"
+theme.menu_fg                   = "#9e9ecb"
+theme.menu_bg                   = "#07090d"
+theme.menu_selected_fg          = "#020508"
 theme.menu_selected_bg          = "#b58900"
-theme.menu_title_bg             = "#002b36"
-theme.menu_primary_title_fg     = "#268bd2"
-theme.menu_secondary_title_fg   = "#dc322f"
+theme.menu_title_bg             = "#020508"
+theme.menu_primary_title_fg     = "#477AB3"
+theme.menu_secondary_title_fg   = "#8C4665"
 
 -- Proxy manager
 theme.proxy_active_menu_fg      = '#000'
@@ -39,31 +39,31 @@ theme.proxy_inactive_menu_fg    = '#888'
 theme.proxy_inactive_menu_bg    = '#FFF'
 
 -- Statusbar specific
-theme.sbar_fg         = "#839496"
-theme.sbar_bg         = "#073642"
+theme.sbar_fg         = "#9e9ecb"
+theme.sbar_bg         = "#07090d"
 
 -- Downloadbar specific
 theme.dbar_fg         = "#808080"
-theme.dbar_bg         = "#073642"
+theme.dbar_bg         = "#07090d"
 theme.dbar_error_fg   = "#F00"
 
 -- Input bar specific
-theme.ibar_fg           = "#839496"
-theme.ibar_bg           = "#073642"
+theme.ibar_fg           = "#9e9ecb"
+theme.ibar_bg           = "#07090d"
 
 -- Tab label
-theme.tab_fg            = "#839496"
-theme.tab_bg            = "#002b36"
-theme.tab_ntheme        = "#839496"
-theme.selected_fg       = "#b58900"
-theme.selected_bg       = "#073642"
-theme.selected_ntheme   = "#839496"
-theme.loading_fg        = "#839496"
-theme.loading_bg        = "#002b36"
+theme.tab_fg            = "#9e9ecb"
+theme.tab_bg            = "#020508"
+theme.tab_ntheme        = "#9e9ecb"
+theme.selected_fg       = "#5e468c"
+theme.selected_bg       = "#07090d"
+theme.selected_ntheme   = "#5e468c"
+theme.loading_fg        = "#9e9ecb"
+theme.loading_bg        = "#020508"
 
 -- Trusted/untrusted ssl colours
-theme.trust_fg          = "#859900"
-theme.notrust_fg        = "#cb4b16"
+theme.trust_fg          = "#287373"
+theme.notrust_fg        = "#bf4d80"
 
 return theme
 -- vim: et:sw=4:ts=8:sts=4:tw=80
